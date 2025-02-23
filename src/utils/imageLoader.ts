6bbc755b1215c127a979bce183472039e5c196a8
ivcode286 <ivcode286@gmail.com>
2025-02-23 19:03:30 +0800

    add new stoires

--- src/utils/imageLoader.ts
+++ src/utils/imageLoader.ts
@@ -5,7 +5,10 @@ const imageMap: Record<string, any> = {
     'fat_cat.jpg': require('../../assets/images/stories_cover/fat_cat.jpg'),
     'panda_river.jpg': require('../../assets/images/stories_cover/panda_river.jpg'),
     'delinquent_cat_sennin.jpg': require('../../assets/images/stories_cover/delinquent_cat_sennin.jpg'),
-    
+    'panda_shrine.jpg': require('../../assets/images/stories_cover/panda_shrine.jpg'),
+    'overlord_food.jpg': require('../../assets/images/stories_cover/overlord_food.jpg'),
+    'girl_hospital_visit.jpg': require('../../assets/images/stories_cover/girl_hospital_visit.jpg'),
+    'colinb_witch.jpg': require('../../assets/images/stories_cover/colinb_witch.jpg'),
   };
   
   export function getImage(imageName: string): any {
