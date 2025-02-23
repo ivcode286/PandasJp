9a8e8764a3fb44742418a91b85fdc2c01a2cc0e8
ivcode286 <ivcode286@gmail.com>
2025-02-23 17:15:34 +0800

    add stories

--- src/utils/imageLoader.ts
+++ src/utils/imageLoader.ts
@@ -1,7 +1,11 @@
 const imageMap: Record<string, any> = {
+  'cover_example.jpg': require('../../assets/images/stories_cover/cover_example.jpg'), // 預設圖
     'momoko.jpg': require('../../assets/images/stories_cover/momoko.jpg'),
     'magical_shoujo.jpg': require('../../assets/images/stories_cover/magical_shoujo.jpg'),
-    'cover_example.jpg': require('../../assets/images/stories_cover/cover_example.jpg'), // 預設圖
+    'fat_cat.jpg': require('../../assets/images/stories_cover/fat_cat.jpg'),
+    'panda_river.jpg': require('../../assets/images/stories_cover/panda_river.jpg'),
+    'delinquent_cat_sennin.jpg': require('../../assets/images/stories_cover/delinquent_cat_sennin.jpg'),
+    
   };
   
   export function getImage(imageName: string): any {
