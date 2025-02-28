0abcec9c993fee99da566625b3d09e1764efa4a1
ivcode286 <ivcode286@gmail.com>
2025-02-28 23:55:28 +0800

    add n5 Kanji ts

---
+++ src/locales/zh-CN/N5KanjiWordsScreen.ts
@@ -0,0 +1,536 @@
+interface N5KanjiCN {
+    wordId: number;
+    words: string;
+    pron: string;
+    letterOrder: number;
+    letter: string;
+    type: string;
+    meaning_zh: string;
+}
+
+const n5KanjiCN: N5KanjiCN[] = [
+    {
+        wordId: 1,
+        words: "一",
+        pron: "ひと(つ)",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "一"
+    },
+    {
+        wordId: 2,
+        words: "二",
+        pron: "ふた(つ)",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "二"
+    },
+    {
+        wordId: 3,
+        words: "三",
+        pron: "みっ(つ)",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "三"
+    },
+    {
+        wordId: 4,
+        words: "四",
+        pron: "よん;よっ(つ)",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "四"
+    },
+    {
+        wordId: 5,
+        words: "五",
+        pron: "いつ(つ)",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "五"
+    },
+    {
+        wordId: 6,
+        words: "六",
+        pron: "むっ(つ)",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "六"
+    },
+    {
+        wordId: 7,
+        words: "七",
+        pron: "なな;なな(つ)",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "七"
+    },
+    {
+        wordId: 8,
+        words: "八",
+        pron: "やっ(つ)",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "八"
+    },
+    {
+        wordId: 9,
+        words: "九",
+        pron: "ここの(つ)",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "九"
+    },
+    {
+        wordId: 10,
+        words: "十",
+        pron: "とお",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "十"
+    },
+    {
+        wordId: 11,
+        words: "百",
+        pron: "ヒャク",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "百"
+    },
+    {
+        wordId: 12,
+        words: "千",
+        pron: "セン",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "千"
+    },
+    {
+        wordId: 13,
+        words: "万",
+        pron: "マン",
+        letterOrder: 1,
+        letter: "数字",
+        type: "数字",
+        meaning_zh: "万"
+    },
+    {
+        wordId: 14,
+        words: "年",
+        pron: "とし",
+        letterOrder: 2,
+        letter: "时间",
+        type: "时间",
+        meaning_zh: "年"
+    },
+    {
+        wordId: 15,
+        words: "月",
+        pron: "つき",
+        letterOrder: 2,
+        letter: "时间",
+        type: "时间",
+        meaning_zh: "月"
+    },
+    {
+        wordId: 16,
+        words: "日",
+        pron: "ひ;か",
+        letterOrder: 2,
+        letter: "时间",
+        type: "时间",
+        meaning_zh: "日"
+    },
+    {
+        wordId: 17,
+        words: "週",
+        pron: "シュウ",
+        letterOrder: 2,
+        letter: "时间",
+        type: "时间",
+        meaning_zh: "周"
+    },
+    {
+        wordId: 18,
+        words: "時",
+        pron: "とき",
+        letterOrder: 2,
+        letter: "时间",
+        type: "时间",
+        meaning_zh: "时间"
+    },
+    {
+        wordId: 19,
+        words: "分",
+        pron: "わ(かる)",
+        letterOrder: 2,
+        letter: "时间",
+        type: "时间",
+        meaning_zh: "分钟;理解"
+    },
+    {
+        wordId: 20,
+        words: "半",
+        pron: "なか(ば)",
+        letterOrder: 2,
+        letter: "时间",
+        type: "时间",
+        meaning_zh: "一半"
+    },
+    {
+        wordId: 21,
+        words: "前",
+        pron: "まえ",
+        letterOrder: 2,
+        letter: "时间",
+        type: "时间",
+        meaning_zh: "之前;前面"
+    },
+    {
+        wordId: 22,
+        words: "後",
+        pron: "あと;うし(ろ)",
+        letterOrder: 2,
+        letter: "时间",
+        type: "时间",
+        meaning_zh: "之后;后面"
+    },
+    {
+        wordId: 23,
+        words: "上",
+        pron: "うえ;あ(がる)",
+        letterOrder: 3,
+        letter: "方位",
+        type: "方位",
+        meaning_zh: "上面"
+    },
+    {
+        wordId: 24,
+        words: "下",
+        pron: "した;さ(がる)",
+        letterOrder: 3,
+        letter: "方位",
+        type: "方位",
+        meaning_zh: "下面"
+    },
+    {
+        wordId: 25,
+        words: "左",
+        pron: "ひだり",
+        letterOrder: 3,
+        letter: "方位",
+        type: "方位",
+        meaning_zh: "左边"
+    },
+    {
+        wordId: 26,
+        words: "右",
+        pron: "みぎ",
+        letterOrder: 3,
+        letter: "方位",
+        type: "方位",
+        meaning_zh: "右边"
+    },
+    {
+        wordId: 27,
+        words: "東",
+        pron: "ひがし",
+        letterOrder: 3,
+        letter: "方位",
+        type: "方位",
+        meaning_zh: "东"
+    },
+    {
+        wordId: 28,
+        words: "西",
+        pron: "にし",
+        letterOrder: 3,
+        letter: "方位",
+        type: "方位",
+        meaning_zh: "西"
+    },
+    {
+        wordId: 29,
+        words: "南",
+        pron: "みなみ",
+        letterOrder: 3,
+        letter: "方位",
+        type: "方位",
+        meaning_zh: "南"
+    },
+    {
+        wordId: 30,
+        words: "北",
+        pron: "きた",
+        letterOrder: 3,
+        letter: "方位",
+        type: "方位",
+        meaning_zh: "北"
+    },
+    {
+        wordId: 31,
+        words: "人",
+        pron: "ひと",
+        letterOrder: 4,
+        letter: "人",
+        type: "人",
+        meaning_zh: "人"
+    },
+    {
+        wordId: 32,
+        words: "友",
+        pron: "とも",
+        letterOrder: 4,
+        letter: "人",
+        type: "人",
+        meaning_zh: "朋友"
+    },
+    {
+        wordId: 33,
+        words: "父",
+        pron: "ちち",
+        letterOrder: 4,
+        letter: "人",
+        type: "人",
+        meaning_zh: "父亲"
+    },
+    {
+        wordId: 34,
+        words: "母",
+        pron: "はは",
+        letterOrder: 4,
+        letter: "人",
+        type: "人",
+        meaning_zh: "母亲"
+    },
+    {
+        wordId: 35,
+        words: "子",
+        pron: "こ",
+        letterOrder: 4,
+        letter: "人",
+        type: "人",
+        meaning_zh: "小孩"
+    },
+    {
+        wordId: 36,
+        words: "男",
+        pron: "おとこ",
+        letterOrder: 4,
+        letter: "人",
+        type: "人",
+        meaning_zh: "男性"
+    },
+    {
+        wordId: 37,
+        words: "女",
+        pron: "おんな",
+        letterOrder: 4,
+        letter: "人",
+        type: "人",
+        meaning_zh: "女性"
+    },
+    {
+        wordId: 38,
+        words: "先生",
+        pron: "セン",
+        letterOrder: 4,
+        letter: "人",
+        type: "人",
+        meaning_zh: "先生;老师"
+    },
+    {
+        wordId: 39,
+        words: "学生",
+        pron: "ガク",
+        letterOrder: 4,
+        letter: "人",
+        type: "人",
+        meaning_zh: "学生"
+    },
+    {
+        wordId: 40,
+        words: "山",
+        pron: "やま",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "山"
+    },
+    {
+        wordId: 41,
+        words: "川",
+        pron: "かわ",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "河川"
+    },
+    {
+        wordId: 42,
+        words: "木",
+        pron: "き",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "树木"
+    },
+    {
+        wordId: 43,
+        words: "花",
+        pron: "はな",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "花"
+    },
+    {
+        wordId: 44,
+        words: "天",
+        pron: "あめ;あま",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "天空"
+    },
+    {
+        wordId: 45,
+        words: "空",
+        pron: "そら",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "空"
+    },
+    {
+        wordId: 46,
+        words: "雨",
+        pron: "あめ",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "雨"
+    },
+    {
+        wordId: 47,
+        words: "雪",
+        pron: "ゆき",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "雪"
+    },
+    {
+        wordId: 48,
+        words: "火",
+        pron: "ひ",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "火"
+    },
+    {
+        wordId: 49,
+        words: "水",
+        pron: "みず",
+        letterOrder: 5,
+        letter: "自然",
+        type: "自然",
+        meaning_zh: "水"
+    },
+    {
+        wordId: 50,
+        words: "本",
+        pron: "もと",
+        letterOrder: 6,
+        letter: "事物",
+        type: "事物",
+        meaning_zh: "书本"
+    },
+    {
+        wordId: 51,
+        words: "車",
+        pron: "くるま",
+        letterOrder: 6,
+        letter: "事物",
+        type: "事物",
+        meaning_zh: "车"
+    },
+    {
+        wordId: 52,
+        words: "門",
+        pron: "かど",
+        letterOrder: 6,
+        letter: "事物",
+        type: "事物",
+        meaning_zh: "门"
+    },
+    {
+        wordId: 53,
+        words: "道",
+        pron: "みち",
+        letterOrder: 6,
+        letter: "事物",
+        type: "事物",
+        meaning_zh: "道路"
+    },
+    {
+        wordId: 54,
+        words: "店",
+        pron: "みせ",
+        letterOrder: 6,
+        letter: "事物",
+        type: "事物",
+        meaning_zh: "商店"
+    },
+    {
+        wordId: 55,
+        words: "食",
+        pron: "た(べる)",
+        letterOrder: 6,
+        letter: "事物",
+        type: "事物",
+        meaning_zh: "吃;食物"
+    },
+    {
+        wordId: 56,
+        words: "飲",
+        pron: "の(む)",
+        letterOrder: 6,
+        letter: "事物",
+        type: "事物",
+        meaning_zh: "喝"
+    },
+    {
+        wordId: 57,
+        words: "物",
+        pron: "もの",
+        letterOrder: 6,
+        letter: "事物",
+        type: "事物",
+        meaning_zh: "物品"
+    },
+    {
+        wordId: 58,
+        words: "音",
+        pron: "おと",
+        letterOrder: 6,
+        letter: "事物",
+        type: "事物",
+        meaning_zh: "声音"
+    }
+];
+
+export default n5KanjiCN;
