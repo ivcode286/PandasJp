b012390c63e1b6712b985b890becac83da9b16f7
ivcode286 <ivcode286@gmail.com>
2025-03-30 13:28:59 +0800

    change kana screens to expo router

--- app/phonetics.tsx
+++ app/phonetics.tsx
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { useTranslation } from "react-i18next";
-import { HiraganaTable } from "./HiraganaScreen";
+import { HiraganaTable } from "./hiragana";
 import useTextToSpeech from "../hooks/useTextToSpeech";
 
 // 定義各個資料項目的型別
