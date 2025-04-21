fda15376d0576ce3ee46ae1d45f65ad23ceef650
ivcode286 <ivcode286@gmail.com>
2025-04-21 14:45:49 +0800

    fix wordscreen warning

--- app/[lang]/words/_layout.tsx
+++ app/[lang]/words/_layout.tsx
@@ -1,12 +1,22 @@
 // app/[lang]/words/_layout.tsx
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Stack } from 'expo-router';
-import React from "react";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function WordsLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="menu" />
-      <Stack.Screen name="[level]" />
-    </Stack>
+    <GestureHandlerRootView style={styles.root}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="menu" />
+        <Stack.Screen name="[level]" />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
